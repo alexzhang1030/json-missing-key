a5d1489672cd5b2ed1bdfd147d9e98778e5d8b6b
alexzhang1030 <1642114555@qq.com>
2023-02-02 15:12:51 +0800

    chore: update

---
+++ src/compare.ts
@@ -0,0 +1,37 @@
+import isObject from 'is-object'
+import objPath from 'object-path'
+import { highlight } from 'codechalk'
+import chalk from 'chalk'
+import type { Props } from './types'
+
+const traverse = (obj: Props['content'], callback: (path: string) => void, parentPath = '') => {
+  for (const key of Object.keys(obj)) {
+    const path = parentPath ? `${parentPath}.${key}` : key
+    callback(path)
+    if (isObject(obj[key]))
+      traverse(obj[key], callback, path)
+  }
+}
+
+const diff = async (base: Props['content'], compareWith: Props['content']) => {
+  const res = {}
+  traverse(compareWith, (path) => {
+    const s = objPath.get(base, path)
+    if (!s)
+      objPath.set(res, path, objPath.get(compareWith, path))
+  })
+  return res
+}
+
+export const compare = async (fileA: Props, fileB: Props) => {
+  const aLeak = await diff(fileA.content, fileB.content)
+  const bLeak = await diff(fileB.content, fileA.content)
+
+  console.log(`${chalk.cyan(fileA.name)} missing keys:\n`)
+  const aOut = await highlight(JSON.stringify(aLeak, null, 2), 'json')
+  console.log(`${aOut}\n\n`)
+
+  console.log(`${chalk.cyan(fileB.name)} missing keys:\n`)
+  const bOut = await highlight(JSON.stringify(bLeak, null, 2), 'json')
+  console.log(bOut)
+}
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { readFile } from 'node:fs/promises'
+import { basename, resolve } from 'node:path'
+import meow from 'meow'
+import consola from 'consola'
+import type { Props } from './types'
+import { compare } from './compare'
+
+const cli = meow(`
+  Usage
+    $ json-missing-key <file1> <file2>
+
+  Examples
+    $ diff-json-key foo.json bar.json
+
+    foo.json missing keys:
+    {
+      "foo": "bar",
+      ...
+    }
+
+    bar.json missing keys:
+    {
+      "bar": "baz"
+    }
+`,
+{
+  importMeta: import.meta,
+})
+
+const main = async (args: string[]) => {
+  if (args.length !== 2)
+    throw new Error('Please provide two files to compare')
+  const res = await Promise.allSettled([
+    readFile(resolve(process.cwd(), args[0]), 'utf-8'),
+    readFile(resolve(process.cwd(), args[1]), 'utf-8'),
+  ])
+  const [fileA, fileB] = res.map((item, index) => {
+    if (item.status === 'rejected') {
+      throw new Error(`Error reading file ${args[index]}`)
+    }
+    else {
+      let content: Props['content']
+      try {
+        content = JSON.parse(item.value)
+      }
+      catch (error) {
+        throw new Error(`Error parsing file ${args[index]}`)
+      }
+      return {
+        name: basename(args[index]),
+        content,
+      } satisfies Props
+    }
+  })
+  compare(fileA, fileB)
+}
+
+try {
+  await main(cli.input)
+}
+catch (error) {
+  consola.error((error as Error).message)
+}
